51f26d35a845f244d7898141448640ed6724772b
aritik755 <aritik755@gmail.com>
2026-06-21 18:30:48 +0530

    Assignment-10

---
+++ ASSIGNMENTS/Assignment-10.js
@@ -0,0 +1,8 @@
+console.log(typeof(10n)); // bigint
+
+const id = Symbol("id")
+console.log(typeof(id)); // symbol
+
+// bigint datatype is used for very large integers. 
+
+console.log(typeof(NaN)); 
