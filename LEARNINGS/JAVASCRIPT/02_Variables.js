5ea6a5dadadecd3bc884a19022883fc11bf79275
aritik755 <aritik755@gmail.com>
2026-06-21 18:26:32 +0530

    Variables in JS

---
+++ LEARNINGS/JAVASCRIPT/02_Variables.js
@@ -0,0 +1,13 @@
+// Keyword to declare variable in javascript(var,let,const).
+let a; // Declaration 
+a = 20; // Initialization
+let name = "Ritik Agarwal"; // Declaration and Initialization in same line.
+// When we use let it cannot use same variable name to declare the variable.
+// When we use var it can be re-assigned or changed/updated or declare using same name.
+var car = "Skoda";
+var car = "Virtus";
+const num = 12;
+// Const is only declared and initialized in same line it do not support declaration and initialization is different lines. 
+// Syntax Error mae code nahi chalenge
+
+
