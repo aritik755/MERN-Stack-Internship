c7f8c85fd6735133b00b0ca7f7e7d2e64b402cd0
aritik755 <aritik755@gmail.com>
2026-06-23 10:13:46 +0530

    Assignment-16.js

---
+++ ASSIGNMENTS/Assignment-16.js
@@ -0,0 +1,11 @@
+const arr = [2, 4, 6, 8, 10];
+
+// firstIndex = 0;
+// lastIndex = arr.length - 1;
+
+let sum = 0;
+for(let i = 0; i < arr.length; i++){
+  sum = sum + arr[i];
+}
+
+console.log(sum);
