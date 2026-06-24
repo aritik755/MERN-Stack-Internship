fd451dc436c4e88c376026dad31670937c1462a2
aritik755 <aritik755@gmail.com>
2026-06-24 10:09:40 +0530

    Assignment-27

---
+++ ASSIGNMENTS/Assignment-27.js
@@ -0,0 +1,11 @@
+// // Function Expression
+// const isEven = function(num) {
+//   return num % 2 === 0;
+// };
+
+// Arrow Function
+const isEven = (num) =>{
+  return num % 2 === 0;
+};
+
+console.log(isEven(12));
