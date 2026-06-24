9ba0790ff3903038037d8fc8cfb2ad04489a46bd
aritik755 <aritik755@gmail.com>
2026-06-24 10:09:35 +0530

    Assignment-26

---
+++ ASSIGNMENTS/Assignment-26.js
@@ -0,0 +1,10 @@
+// Function Definition
+// function calculateArea(width, height){
+//   return width * height;
+// }
+
+// To Function Expression
+const calculateArea = function(width, height){
+  return width * height;
+}
+console.log(calculateArea(3,4));
