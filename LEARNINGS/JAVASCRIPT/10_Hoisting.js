fc34fc06eff25cbd30ec87af6464d0d0fcd82d95
aritik755 <aritik755@gmail.com>
2026-06-26 09:58:01 +0530

    Hoisting in JS

---
+++ LEARNINGS/JAVASCRIPT/10_Hoisting.js
@@ -0,0 +1,12 @@
+// Hoisting
+
+// JS CODE --> JS ENGINE --> BROWSER --> JS
+
+// 1. Prasing : Code Scanning(AST)
+// 2. Compilation : Binary Code 
+// 3. Execution: 
+// a. Memory Allocation (Memory Allocation is AST into Binary Code)
+// b. Execution
+
+// var variable and only function declaration are hoisted.
+// hoisted move the declaration part to the top of your code.
