489565cf6a95a9b87d78c3c702cbe92713eaddd5
aritik755 <aritik755@gmail.com>
2026-06-27 10:03:40 +0530

    Assignment-46

---
+++ ASSIGNMENTS/Assignment-46.js
@@ -0,0 +1,11 @@
+const person = {
+  name: "John",
+  age: 25,
+  city: "New York"
+};
+ 
+// Extract name and age using object destructuring.
+
+let{name , age} = person;
+console.log(name);
+console.log(age);
